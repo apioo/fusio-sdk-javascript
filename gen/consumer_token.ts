2531ff1353cde162143ee471c48bb986befa41b6
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-05-19 11:13:20 +0200

    use oauth2 resource

--- gen/consumer_token.ts
+++ gen/consumer_token.ts
@@ -1,58 +1,8 @@
-import Axios, {AxiosInstance, AxiosPromise} from "axios";
+import {AxiosInstance} from "axios";
+import Oauth2Resource from "../src/Oauth2Resource";
 
-export default class Resource {
-    private url: string;
-    private token: string;
-    private httpClient: AxiosInstance;
-
-
+export default class Resource extends Oauth2Resource {
     public constructor(baseUrl: string, token: string, httpClient?: AxiosInstance) {
-
-        this.url = baseUrl + "/consumer/token";
-        this.token = token;
-        this.httpClient = httpClient ? httpClient : Axios.create();
+        super("/consumer/token", baseUrl, token, httpClient);
     }
-
-    public post(data: Authorization_code | Password | Client_credentials | Refresh_token): AxiosPromise<Access_token> {
-        let params = {
-            method: "POST",
-        };
-
-        return this.httpClient.post<Access_token>(this.url, data, params);
-    }
-
 }
-
-interface Endpoint {
-    Authorization?: Authorization_code | Password | Client_credentials | Refresh_token
-    Access_token?: Access_token
-}
-interface Authorization_code {
-    grant_type: string
-    code: string
-    redirect_uri?: string
-    client_id?: string
-}
-interface Password {
-    grant_type: string
-    username: string
-    password: string
-    scope?: string
-}
-interface Client_credentials {
-    grant_type: string
-    scope?: string
-}
-interface Refresh_token {
-    grant_type: string
-    refresh_token: string
-    scope?: string
-}
-interface Access_token {
-    access_token?: string
-    token_type?: string
-    expires_in?: string
-    refresh_token?: string
-}
-
-
