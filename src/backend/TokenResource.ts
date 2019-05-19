4f1713adccf72fe5bb1c83b5a15428751e56693f
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-05-19 02:03:19 +0200

    add backend token resource

---
+++ src/backend/TokenResource.ts
@@ -0,0 +1,10 @@
+import * as Token from "../../gen/backend_token";
+import {BaseResource} from "../BaseResource";
+
+export class TokenResource extends BaseResource
+{
+    public do()
+    {
+        return new Token.default(this.baseUrl, this.accessToken, this.httpClient);
+    }
+}
