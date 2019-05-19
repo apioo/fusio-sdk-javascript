87f4d42181c121d6c567d4856492d1b77b33fc7d
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-05-19 11:12:54 +0200

    add setter to base url, access token and http client

--- src/BaseResource.ts
+++ src/BaseResource.ts
@@ -1,15 +1,25 @@
 import {AxiosInstance} from "axios";
 
-export class BaseResource
-{
+export class BaseResource {
     protected baseUrl: string;
     protected accessToken: string;
     protected httpClient: AxiosInstance;
 
-    public constructor(baseUrl: string, accessToken: string, httpClient: AxiosInstance)
-    {
+    public constructor(baseUrl: string, accessToken: string, httpClient: AxiosInstance) {
         this.baseUrl = baseUrl;
         this.accessToken = accessToken;
+        this.httpClient = httpClient;
+    }
+
+    public setBaseUrl(baseUrl: string) {
+        this.baseUrl = baseUrl;
+    }
+
+    public setAccessToken(accessToken: string) {
+        this.accessToken = accessToken;
+    }
+
+    public setHttpClient(httpClient: AxiosInstance) {
         this.httpClient = httpClient;
     }
 }
